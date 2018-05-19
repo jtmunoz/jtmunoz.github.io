7341ae7f6a02f3480469743842b281a6f3c417cb
jtmunoz <jordantmunoz@gmail.com>
2018-05-18 20:44:34 -0700

    udpates code

--- javascript/map.js
+++ javascript/map.js
@@ -6,13 +6,16 @@ function geoFindMe() {
   }
 
   function success(position) {
+    $()
     var latitude  = position.coords.latitude;
     var longitude = position.coords.longitude;
     output.innerHTML = '<p>Latitude is ' + latitude + '° <br>Longitude is ' + longitude + '°</p>';
-    var img = new Image('50vw','50vw');
-    img.src = "https://maps.googleapis.com/maps/api/staticmap?center=" + latitude + "," + longitude + "&zoom=13&size=300x300&sensor=false";
+    var img = new Image();
+    img.src = "https://maps.googleapis.com/maps/api/staticmap?center=" + latitude + "," + longitude + "&zoom=13&size=1000x1000&sensor=false";
     output.appendChild(img);
   }
+
+  $('#out img').css("width", "50vw");
 
   function error() {
     output.innerHTML = "Unable to retrieve your location";
