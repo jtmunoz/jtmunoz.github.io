6e679f4c2159c11efc8aa1a1077b637550e1fab6
jtmunoz <jordantmunoz@gmail.com>
2018-05-18 18:38:48 -0700

    removes extra map page, updates current map page

--- javascript/map.js
+++ javascript/map.js
@@ -9,7 +9,7 @@ function geoFindMe() {
     var latitude  = position.coords.latitude;
     var longitude = position.coords.longitude;
     output.innerHTML = '<p>Latitude is ' + latitude + '° <br>Longitude is ' + longitude + '°</p>';
-    var img = new Image();
+    var img = new Image('50vw','50vw');
     img.src = "https://maps.googleapis.com/maps/api/staticmap?center=" + latitude + "," + longitude + "&zoom=13&size=300x300&sensor=false";
     output.appendChild(img);
   }
